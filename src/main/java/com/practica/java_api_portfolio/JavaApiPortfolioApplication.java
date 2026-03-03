133e74cd3019f31cd09fb2296a1fb36926eb75a2
Eduardo Castro <eduardocastroramon0@gmail.com>
2026-03-02 22:24:10 -0600

    FEAT:
      - Configuracion inicial del proyecto

---
+++ src/main/java/com/practica/java_api_portfolio/JavaApiPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.practica.java_api_portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApiPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApiPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practica/java_api_portfolio/JavaApiPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.java_api_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApiPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
