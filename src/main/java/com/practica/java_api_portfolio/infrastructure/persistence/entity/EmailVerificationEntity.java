5bd6e7bdc1604baad136acda510b576145450d42
Eduardo <eduardo.castro@syesoftware.com>
2026-03-17 16:54:24 -0600

    FEAT:   - Adicion de entidad

---
+++ src/main/java/com/practica/java_api_portfolio/infrastructure/persistence/entity/EmailVerificationEntity.java
@@ -0,0 +1,40 @@
+package com.practica.java_api_portfolio.infrastructure.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Table(name = "email_verifications")
+@Data
+@Entity
+public class EmailVerificationEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @OneToOne(optional = false)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", unique = true, nullable = false)
+    private UserEntity user;
+
+    @Column(name = "code_hash", nullable = false)
+    private String codeHash;
+
+    @Column(name = "expires_at", nullable = false)
+    private Instant expiresAt;
+
+    @Column(nullable = false)
+    private int attempts;
+
+    @Column(name = "last_sent_at", nullable = false)
+    private Instant lastSentAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.attempts = 0;
+        this.expiresAt = Instant.now();
+        this.codeHash = "";
+        this.lastSentAt = Instant.now();
+    }
+}
