82ec4e973d9cb0b9bd2689e0039efed450f10763
Eduardo <eduardo.castro@syesoftware.com>
2026-03-24 12:37:57 -0600

    FEAT:   - Modificación del `ApiExceptionHandler` para el manejo de excepciones personalizadas

--- src/main/java/com/practica/java_api_portfolio/api/exception/ApiExceptionHandler.java
+++ src/main/java/com/practica/java_api_portfolio/api/exception/ApiExceptionHandler.java
@@ -1,23 +1,35 @@
 package com.practica.java_api_portfolio.api.exception;
 
+import com.practica.java_api_portfolio.api.dto.Error;
+import com.practica.java_api_portfolio.application.exception.EmailNotUniqueException;
+import com.practica.java_api_portfolio.application.exception.UsernameNotUniqueException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.Instant;
-import java.util.Map;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Error> internalError(Exception ex) {
+        var error = new Error(ex.getMessage());
+        return new ResponseEntity<Error>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
     @ExceptionHandler(IllegalArgumentException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Map<String, Object> notFound(IllegalArgumentException ex) {
-        return Map.of(
-                "timestamp", Instant.now().toString(),
-                "error", "NOT_FOUND",
-                "message", ex.getMessage()
-        );
+    public ResponseEntity<Error> inputNoValid(IllegalArgumentException ex) {
+        var error = new Error(ex.getMessage());
+        return new ResponseEntity<Error>(error, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(UsernameNotUniqueException.class)
+    public final ResponseEntity<Error> usernameNotUnique(UsernameNotUniqueException ex) {
+        var error = new Error(ex.getMessage());
+        return new ResponseEntity<Error>(error, HttpStatus.CONFLICT);
+    }
+    @ExceptionHandler(EmailNotUniqueException.class)
+    public final ResponseEntity<Error> emailNotUnique(EmailNotUniqueException ex) {
+        var error = new Error(ex.getMessage());
+        return new ResponseEntity<Error>(error, HttpStatus.CONFLICT);
     }
 }
